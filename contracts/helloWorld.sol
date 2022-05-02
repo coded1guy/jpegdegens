279f06ccd739734cea5d366156d55e152808ba8e
coded1guy <grggmmnl@gmail.com>
2022-05-02 02:53:02 +0100

    Create helloWorld.sol

---
+++ contracts/helloWorld.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+contract helloWorld {
+    string next = "Boruto - Naruto Next Generations - 171 [720p x265]";
+    function getter() public view returns(string memory) {
+        return next;
+    }
+}
