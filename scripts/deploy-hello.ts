23f556455d4a87b3b786460f1402261c251135d9
coded1guy <grggmmnl@gmail.com>
2022-05-02 14:51:41 +0100

    created a deployment script

---
+++ scripts/deploy-hello.ts
@@ -0,0 +1,19 @@
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+
+async function foo() {
+    const HelloWorld = await ethers.getContractFactory("helloWorld");
+    const hello = await HelloWorld.deploy();
+    await hello.deployed();
+    return hello;
+}
+async function deploy() {
+    const hello = await foo();
+    return hello;
+}
+//@ts-ignore
+async function sayHello(hello) {
+    console.log("Say hello: ", await hello.getter())
+}
+
+deploy().then(sayHello);
